28c115f173de4497d1db3642b15c012a14d88d7f
jaumeortola <jaumeortola@gmail.com>
2023-10-27 20:30:42 +0200

    improve filter wikicorpus

--- src/main/java/softcatala/filterwikicorpus/filterwikicorpus.java
+++ src/main/java/softcatala/filterwikicorpus/filterwikicorpus.java
@@ -31,25 +31,31 @@ public class filterwikicorpus {
   private static int SENTENCE_CHAR_MIN_SIZE = 12;
   private static final Pattern VALID_SENTENCE = Pattern.compile("^[«»]?[A-ZÀÈÉÍÒÓÚ].+[\\.!?]$");
   private static final Pattern INVALID_SENTENCE = Pattern
-      .compile(".*([0-9]|[A-Z]\\.|art\\.|núm\\.|[A-ZÀÈÉÍÒÓÚ][A-ZÀÈÉÍÒÓÚ]|[\\(\\)\\[\\]śźń:]).*");
+      .compile(".*(\\*|[0-9]|[A-Z]\\.|art\\.|núm\\.|[A-ZÀÈÉÍÒÓÚ][A-ZÀÈÉÍÒÓÚ]|[\\(\\)\\[\\]śźń:]).*");
   static ResultCache cache = null;
   static UserConfig userConfig = new UserConfig(new ArrayList<String>(), new HashMap<String, Integer>());
   private static JLanguageTool langTool = new JLanguageTool(new Catalan(), cache, userConfig);
 
   public static void main(String[] args) throws Exception {
-    if (args.length != 3) {
-      System.err.println("Use: java -jar filterwikicorpus.jar --json json_files_path ouputfile");
-      System.err.println("Use: java -jar filterwikicorpus.jar --txt text_inputfile ouputfile");
-      System.exit(1);
-    }
-    String option = args [0];
-    String outputFilename = args[2];
+    /*if (args.length != 3) {
+      exitProgram();
+    }*/
+    //String option = args [0];
+    //String outputFilename = args[2];
+    
+    //if (!option.equals("--txt") && !option.equals("--json")) {
+    //  exitProgram();
+    //}
 
+    String outputFilename= "/home/jaume/europarl-eng-spa-cat/selected/cat-spa.txt";
+    String option = "--txt";
+    
     PrintWriter out = new PrintWriter(outputFilename);
     
     langTool.disableRules(Arrays.asList("EXIGEIX_VERBS_CENTRAL", "EXIGEIX_ACCENTUACIO_GENERAL", "EXIGEIX_POSSESSIUS_V",
         "EVITA_PRONOMS_VALENCIANS", "EVITA_DEMOSTRATIUS_EIXE", "VOCABULARI_VALENCIA", "EXIGEIX_US" 
-        ,"SER_ESSER", "WHITESPACE_RULE", "CA_SIMPLEREPLACE_DIACRITICS_IEC"
+        ,"SER_ESSER", "WHITESPACE_RULE",
+        "CA_UNPAIRED_BRACKETS", "ESPAIS_SOBRANTS, MAJ_DESPRES_INTERROGANT,UPPERCASE_SENTENCE_START"
         ));
 
     if (option.equals("--json")) {
@@ -82,7 +88,7 @@ public class filterwikicorpus {
                 continue;
               }
               if ((str.length() > SENTENCE_CHAR_MIN_SIZE) && (str.length() < SENTENCE_CHAR_MAX_SIZE)
-                  && (!str.endsWith("...")) && (VALID_SENTENCE.matcher(str).matches())
+                  //&& (!str.endsWith("...")) && (VALID_SENTENCE.matcher(str).matches())
                   && (!INVALID_SENTENCE.matcher(str).matches()) && (langTool.check(str).isEmpty())) {
                 out.println(str);
                 allsentences.add(str);
@@ -98,37 +104,53 @@ public class filterwikicorpus {
       }
     }
     
-    if (option.equals("--txt")) {
-        String inputFilename = args[1];
-        List<String> allsentences = new ArrayList<String>();
-        try (BufferedReader br = new BufferedReader(new FileReader(inputFilename))) {
-          String line;
-          while ((line = br.readLine()) != null) {
-            List<String> sentences = lang.getSentenceTokenizer().tokenize(line);
-            for (String s : sentences) {
-              String str = s.trim();
-              /*if (str.startsWith("--"))
-                str = str.substring(2);
-              if (str.startsWith("-") || str.startsWith("—") || str.startsWith("–") || str.startsWith("»")) {
-                str = str.substring(1);
-              }*/
-              str = s.trim();
-              if (allsentences.contains(str)) {
-                System.err.println(str);
-                continue;
-              }
-              if ( /*(str.length() > SENTENCE_CHAR_MIN_SIZE) && (str.length() < SENTENCE_CHAR_MAX_SIZE)
-                  && (!str.endsWith("...")) && (VALID_SENTENCE.matcher(str).matches())
-                  && (!INVALID_SENTENCE.matcher(str).matches()) &&*/ (langTool.check(str).isEmpty())) {
-                out.println(str);
-                allsentences.add(str);
+    else if (option.equals("--txt")) {
+      //String inputFilename = args[1];
+      List<String> allsentences = new ArrayList<String>();
+      for (int k=0; k<4; k++) {
+        
+        String inputFilename = "/home/jaume/europarl-eng-spa-cat/generat/cat0" + k;
+        String spanishFilename = "/home/jaume/europarl-eng-spa-cat/revisat/spa0" + k;
+        
+        try (BufferedReader brSpa = new BufferedReader(new FileReader(spanishFilename))) {
+          try (BufferedReader br = new BufferedReader(new FileReader(inputFilename))) {
+            String line;
+            String lineSpa;
+            while ((line = br.readLine()) != null) {
+              lineSpa = brSpa.readLine();
+              //List<String> sentences = lang.getSentenceTokenizer().tokenize(line);
+              //for (String s : sentences) {
+              String s = line; {
+                //System.err.println(s);
+                String str = s.trim();
+                if (str.startsWith("--"))
+                  str = str.substring(2);
+                if (str.startsWith("-") || str.startsWith("—") || str.startsWith("–") || str.startsWith("»")) {
+                  str = str.substring(1);
+                }
+                str = s.trim();
+                if (allsentences.contains(str)) {
+                  //System.err.println(str);
+                  continue;
+                }
+                if ( (str.length() > SENTENCE_CHAR_MIN_SIZE) && (str.length() < SENTENCE_CHAR_MAX_SIZE)
+                    //&& (!str.endsWith("...")) && (VALID_SENTENCE.matcher(str).matches())
+                    && (!INVALID_SENTENCE.matcher(str).matches()) && 
+                    langTool.check(str).isEmpty()
+                    //langTool.check(str).size() == 1
+                    ) {
+                  out.println(str+"\t" + lineSpa.trim());
+                  allsentences.add(str);
 
-              } else {
-                System.err.println(str);
+                } else {
+                  //System.err.println(str);
+                }
               }
             }
           }
         }
+        
+      }
 
     }
     
@@ -149,5 +171,10 @@ public class filterwikicorpus {
     }
     return fileNames;
   }
-
+  
+  private static void exitProgram(){
+    System.err.println("Use: java -jar filterwikicorpus.jar --json json_files_path ouputfile");
+    System.err.println("Use: java -jar filterwikicorpus.jar --txt text_inputfile ouputfile");
+    System.exit(1);
+  }
 }
